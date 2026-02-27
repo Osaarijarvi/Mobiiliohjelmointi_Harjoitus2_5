5b45522e29bf01364e4a0488d15cc1dcc84e4842
Outi Saarijärvi <osaarija@edu.lapinamk.fi>
2026-02-27 20:01:33 +0200

    Task 2_5: empty project created

---
+++ app/src/main/java/com/example/mobiiliohjelmointi_harjoitus2_5/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.mobiiliohjelmointi_harjoitus2_5
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobiiliohjelmointi_harjoitus2_5.ui.theme.Mobiiliohjelmointi_Harjoitus2_5Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Mobiiliohjelmointi_Harjoitus2_5Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Mobiiliohjelmointi_Harjoitus2_5Theme {
+        Greeting("Android")
+    }
+}
