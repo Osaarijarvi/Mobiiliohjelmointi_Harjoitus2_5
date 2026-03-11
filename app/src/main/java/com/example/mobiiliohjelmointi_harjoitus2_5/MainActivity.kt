704b48f5c708780ccb3cfb1a1d7914a77bd60984
Outi Saarijärvi <osaarija@edu.lapinamk.fi>
2026-03-11 21:30:08 +0200

    Task 2_5: - Layout OK - Could not fix Countscore - StartOverButton added, does not work - Code for disabling and enabling buttons added, does not work

--- app/src/main/java/com/example/mobiiliohjelmointi_harjoitus2_5/MainActivity.kt
+++ app/src/main/java/com/example/mobiiliohjelmointi_harjoitus2_5/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mobiiliohjelmointi_harjoitus2_5
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -19,6 +21,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,10 +33,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.test.isEnabled
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.example.mobiiliohjelmointi_harjoitus2_5.ui.theme.Mobiiliohjelmointi_Harjoitus2_5Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +47,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Mobiiliohjelmointi_Harjoitus2_5Theme {
-                DiceCastApp()
+                    DiceCastApp()
             }
         }
     }
@@ -59,58 +65,101 @@ val dices = listOf(
 
 @Composable
 fun DiceCastApp() {
+    var castCounter by remember() { mutableStateOf(0) }
     var totalScore by remember() { mutableStateOf(0) }
     var diceDots1 by remember() { mutableStateOf(0) }
     var diceDots2 by remember() { mutableStateOf(0) }
 
-    Column(modifier = Modifier.padding(24.dp).fillMaxSize(),
+    Column(
+        modifier = Modifier.padding(24.dp).fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-
         DiceImage(diceDots1, diceDots2)
 
         Spacer(modifier = Modifier.height(32.dp))
 
-        CastButton(
-            onRandom = {
-                // Numbers matching the dots of both dices are raffled
-                diceDots1 = (1..6).random()
-                diceDots2 = (1..6).random()
-            }
-        )
-        CountScore (diceDots1, diceDots2,
+        if(castCounter <4 ) {
+            CastButton(
+                castCounter = castCounter,
+                enabledCastButton = true,
+                onRandom = {
+                    // When the function is called, counter goes up with 1
+                    castCounter = castCounter++
+
+                    // Numbers matching the dots of both dices are raffled
+                    diceDots1 = (1..6).random()
+                    diceDots2 = (1..6).random()
+                }
+            )
+        }
+
+        CountScore(
+            diceDots1, diceDots2,
             addScore = { diceScore: Int ->
-                Log.d("diceScore", diceScore.toString())
-                totalScore += diceScore
+                //Log.d("diceScore", diceScore.toString())
+                totalScore = totalScore + diceScore
             })
 
-        Row (
+        Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly)
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "Pisteet",
+                modifier = Modifier.background(Color.DarkGray)
+                    .padding(vertical = 2.dp, horizontal = 8.dp),
+                fontSize = 20.sp
+            )
+            Text(
+                text = "$totalScore",
+                modifier = Modifier.background(Color.Green)
+                    .padding(vertical = 2.dp, horizontal = 8.dp),
+                fontSize = 20.sp
+            )
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        // Game is reset after three casts
+        if (castCounter == 4)
         {
-        Text(
-            text = "Pisteet",
-            modifier = Modifier.background(Color.DarkGray).
-            padding(vertical = 2.dp, horizontal = 8.dp),
-            fontSize = 20.sp
-        )
-        Text(text = "$totalScore",
-            modifier = Modifier.background(Color.Green).
-            padding(vertical = 2.dp, horizontal = 8.dp),
-            fontSize = 20.sp)
+            StartAgainButton(
+                enabledStartAgainButton = true,
+                startOver = {
+                    enabledCastButton = false
+                    castCounter = 0
+                    totalScore = 0
+                    diceDots1 = 0
+                    diceDots2 = 0
+                }
+            )
         }
     }
 }
 
 @Composable
-fun CastButton(
-    onRandom: () -> Unit)
-{
+fun CastButton(enabledCastButton : Boolean,
+               castCounter: Int,
+               onRandom: () -> Unit
+) {
     Button(
+        enabled = enabledCastButton,
         onClick = onRandom
     ) {
         Text(text = "Heitä", fontSize = 24.sp)
+    }
+}
+@Composable
+fun StartAgainButton(
+    enabledStartAgainButton : Boolean,
+    startOver: () -> Unit
+) {
+    Button(
+        enabled = enabledStartAgainButton,
+        onClick = startOver
+    ) {
+        Text(text = "Aloita Alusta", fontSize = 24.sp)
     }
 }
 
@@ -135,22 +184,29 @@ fun DiceImage(diceDots1: Int, diceDots2: Int) {
 
 @Composable
 fun CountScore(
-    diceDots1: Int, diceDots2 : Int,
+    diceDots1: Int, diceDots2: Int,
     addScore: (diceScore: Int) -> Unit
 ) {
     Row(
         modifier = Modifier.fillMaxWidth().padding(8.dp),
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
+        // When both dices have the same number of dots, sum of dots
+        // will be added to the total score.
         if (diceDots1 == diceDots2) {
             addScore(diceDots1 + diceDots2)
         }
+        // When the first dice has higher number of dots, its number of dots
+        // will be added to the total score.
         if (diceDots1 > diceDots2) {
             addScore(diceDots1)
         }
+        // When the second dice has higher number of dots, its number of dots
+        // will be added to the total score.
         if (diceDots2 > diceDots1) {
             addScore(diceDots2)
         }
     }
-
 }
+
+
